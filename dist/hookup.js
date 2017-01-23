fb4ecd5ab31bf07fa02dca609e6523f2a3a98a17
Austin Billings <austinjosephbillings@gmail.com>
2017-01-23 01:19:29 -0500

    remove annoying alert

--- dist/hookup.js
+++ dist/hookup.js
@@ -18,7 +18,7 @@ angular.module('Hookup').directive('googleAnalytics', function($timeout) {
 			attrs.$observe('key', function (key) {
 				var tag = document.createElement("script");
 				tag.setAttribute('type','text/javascript');
-				tag.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + key + "', 'auto');ga('send', 'pageview');alert('ran !');";
+				tag.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + key + "', 'auto');ga('send', 'pageview');";
 				element.append(tag);
 			});
 		}
